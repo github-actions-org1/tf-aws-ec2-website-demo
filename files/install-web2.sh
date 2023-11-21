7df2b2c968e8ff8cdeb3bb62c889d035017fa3df
swilliams <solomon.fwilliams@gmail.com>
2023-11-21 10:56:29 -0600

    new

--- files/install-web2.sh
+++ files/install-web2.sh
@@ -4,7 +4,7 @@ sudo yum -y install httpd git
 cd /tmp
 git clone https://github.com/cicd-github-action/ecommerce.git
 sleep 15s
-cp -r /tmp/ecommerce/realestate/* /var/www/html/
+cp -r /tmp/ecommerce/homeverse/* /var/www/html/
 sudo systemctl stop httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
